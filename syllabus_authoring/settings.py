f61c2adc5b65f5b73f03c79d54d73ef1ced877f2
Pranavaaaaaaa <bharadwajpranav05@gmail.com>
2026-04-29 11:00:06 +0530

    Shrikrishnarpanamastu

--- syllabus_authoring/settings.py
+++ syllabus_authoring/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a%g0$u+m**1!h7bb(jf&xrzpr6)(z95g&)sx7lg^x_qs$73bfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
